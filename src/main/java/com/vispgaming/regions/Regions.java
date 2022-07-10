2667e8c4be911d662c3d0118edbbb5572bce023d
VIspReaderUS <butler8302@live.com>
2022-07-10 18:48:31 -0400

    initial commit

---
+++ src/main/java/com/vispgaming/regions/Regions.java
@@ -0,0 +1,14 @@
+package com.vispgaming.regions;
+
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(Regions.MOD_ID)
+public class Regions {
+  public static final String MOD_ID = "regions";
+  public static final Logger LOGGER = LogManager.getLogger();
+  public Regions() {
+    LOGGER.info("testTESTtestTESTtestTESTtestTESTtestTESTtestTESTtestTEST");
+  }
+}
